f325a5ed51aa14777e201d70ddb4afa548ca7ac9
MasonWang025 <mason.wang0025@gmail.com>
2020-09-01 21:16:31 -0700

    major component refactoring

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,70 +35,23 @@ export default function Header(props) {
             <Typography variant="h6" className={classes.title}>
               MUI Boilerplate
             </Typography>
-            {/* for large */}
-            <Hidden xsDown>
-              {routes.map((route) => (
-                <Link to={route.path} key={route.path + route.name}>
-                  <Button
-                    className={`${classes.navLinks} ${
-                      currPath === route.path && "active"
-                    }`}
-                  >
-                    {route.name}
-                  </Button>
-                </Link>
-              ))}
-            </Hidden>
-            {/* for mobile */}
-            <Hidden smUp>
-              <IconButton
-                edge="start"
-                className={classes.menuButton}
-                color="inherit"
-                aria-label="menu"
-                style={{ marginRight: 0 }}
-                onClick={() => setDrawerOpen(true)}
-              >
-                <MenuIcon />
-              </IconButton>
-
-              <Drawer
-                elevation={0}
-                anchor="right"
-                open={drawerOpen}
-                onClose={() => setDrawerOpen(false)}
-              >
-                <div
-                  role="presentation"
-                  onClick={() => setDrawerOpen(false)}
-                  onKeyDown={() => setDrawerOpen(false)}
-                >
-                  <List className={classes.list}>
-                    {routes.map((route) => (
-                      <Link key={route.path} to={route.path}>
-                        <ListItem className={classes.listItem} button>
-                          <ListItemText primary={route.name} />
-                        </ListItem>
-                      </Link>
-                    ))}
-                  </List>
-                </div>
-              </Drawer>
-            </Hidden>
+            <DesktopNav classes={classes} currPath={currPath} />
+            <MobileNav
+              classes={classes}
+              currPath={currPath}
+              drawerOpen={drawerOpen}
+              setDrawerOpen={setDrawerOpen}
+            />
           </Toolbar>
         </Container>
       </AppBar>
       <Toolbar id="back-to-top-anchor" />
-      <ScrollTop {...props}>
-        <Fab color="secondary" size="medium" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon />
-        </Fab>
-      </ScrollTop>
+      <BackTop classes={classes} {...props} />
     </React.Fragment>
   );
 }
 
-function ScrollTop(props) {
+function BackTop(props) {
   const { children } = props;
   const classes = useStyles();
 
@@ -118,8 +71,69 @@ function ScrollTop(props) {
   return (
     <Zoom in={trigger}>
       <div onClick={handleClick} className={classes.backToTop}>
-        {children}
+        <Fab color="secondary" size="medium" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
       </div>
     </Zoom>
+  );
+}
+
+// NAVIGATION COMPONENTS
+function DesktopNav({ classes, currPath }) {
+  return (
+    <Hidden xsDown>
+      {routes.map((route) => (
+        <Link to={route.path} key={route.path + route.name}>
+          <Button
+            className={`${classes.navLinks} ${
+              currPath === route.path && "active"
+            }`}
+          >
+            {route.name}
+          </Button>
+        </Link>
+      ))}
+    </Hidden>
+  );
+}
+
+function MobileNav({ classes, currPath, drawerOpen, setDrawerOpen }) {
+  return (
+    <Hidden smUp>
+      <IconButton
+        edge="start"
+        className={classes.menuButton}
+        color="inherit"
+        aria-label="menu"
+        style={{ marginRight: 0 }}
+        onClick={() => setDrawerOpen(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+
+      <Drawer
+        elevation={0}
+        anchor="right"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <div
+          role="presentation"
+          onClick={() => setDrawerOpen(false)}
+          onKeyDown={() => setDrawerOpen(false)}
+        >
+          <List className={classes.list}>
+            {routes.map((route) => (
+              <Link key={route.path} to={route.path}>
+                <ListItem className={classes.listItem} button>
+                  <ListItemText primary={route.name} />
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </div>
+      </Drawer>
+    </Hidden>
   );
 }
