05d0be74638c0ab409497b036514bce2bcb46767
MasonWang025 <mason.wang0025@gmail.com>
2020-09-06 10:11:52 -0700

    join the team

---
+++ src/components/SectionAbout.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Box, Grid, Typography } from "@material-ui/core";
+
+export default function SectionAbout(props) {
+  return (
+    <Box id="home-about" py={5} id={props.id}>
+      <Container fixed>
+        <Typography align="center" variant="h4">
+          <b>{props.title}</b>
+        </Typography>
+        <Box mt={1}>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justify="center"
+            spacing={2}
+          >
+            <Grid item>
+              <Typography align="center" variant="body1">
+                {props.content}
+              </Typography>
+            </Grid>
+            <Grid item>{props.callToAction}</Grid>
+            {props.children && <Grid item>{props.children}</Grid>}
+          </Grid>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,21 +1,67 @@
 import React from "react";
 import Page from "../components/Page";
 import SectionHero from "../components/SectionHero";
-import { Button } from "@material-ui/core";
+import SectionAbout from "../components/SectionAbout";
+import { Button, Typography } from "@material-ui/core";
 
 export default function Team() {
   return (
     <Page>
       <SectionHero
         title="Meet our team"
-        paragraph="Here is a short paragraph that has the page's key information."
+        paragraph="Get to know the team that makes the magic happen."
         imgURL="https://res.cloudinary.com/masonwang/image/upload/v1599405239/yei-website/manypixels-isometric/teamHero.png"
         callToAction={
-          <Button variant="contained" color="secondary">
-            Join Team
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              document
+                .querySelector("#join-the-team")
+                .scrollIntoView({ behavior: "smooth", block: "center" });
+            }}
+          >
+            Apply Now
           </Button>
         }
       />
+      <SectionAbout
+        title="Join the Team"
+        id="join-the-team"
+        content={
+          <span>
+            Passionate about what we do? Want to join an international team of
+            leaders, creators, and economics enthusiasts? You can find more
+            information in the document below:
+          </span>
+        }
+        callToAction={
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://docs.google.com/document/d/1VBmep4hRFlc_m34zoB3PrCpR41UnlAKgH2r-k3FBrAM/edit"
+          >
+            <Button
+              align="center"
+              variant="contained"
+              color="primary"
+              size="large"
+            >
+              Apply Now
+            </Button>
+          </a>
+        }
+      >
+        <Typography align="center" variant="body2">
+          Note that this application process is highly competitive with a{" "}
+          <i>{"<10%"} acceptance rate</i> for several positions — not all
+          qualified applicants receive a position because we simply do not have
+          the space! Applicants may be invited to several rounds of interviews
+          before a final decision is made. We employ a holistic review process
+          for each applicant and are looking for students who exhibit high
+          levels of drive, initiative, enthusiasm, and commitment.
+        </Typography>
+      </SectionAbout>
     </Page>
   );
 }
