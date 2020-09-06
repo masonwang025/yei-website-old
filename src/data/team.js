e621af8663679f830623fb739e90c36367b95617
MasonWang025 <mason.wang0025@gmail.com>
2020-09-06 12:49:23 -0700

    all document members added to data/team.js

---
+++ src/data/team.js
@@ -0,0 +1,190 @@
+const team = [
+  {
+    name: "Henry Weng",
+    positions: ["CEO", "Co-founder", "Co-Chairman"],
+    linkedin: "https://www.linkedin.com/in/henry-weng/",
+  },
+  {
+    name: "Ian Chen",
+    positions: ["Co-founder", "COO", "President", "Co-Chairman"],
+    linkedin: "https://www.linkedin.com/in/iandchen/",
+  },
+  {
+    name: "Andy Chen",
+    positions: ["Executive Board Member", "VP of Leadership"],
+    linkedin: "https://www.linkedin.com/in/andy-chen-82ba6b1a9/",
+  },
+  {
+    name: "Andy Chen",
+    positions: ["Executive Board Member", "VP of Leadership"],
+    linkedin: "https://www.linkedin.com/in/andy-chen-82ba6b1a9/",
+  },
+  {
+    name: "Anishka Banerjee",
+    positions: ["Executive Board Member", "VP of Finance"],
+    linkedin: "https://www.linkedin.com/in/anishka-banerjee-657775171/",
+  },
+  {
+    name: "Cheryl Wu",
+    positions: ["Executive Board Member", "VP of Events"],
+    linkedin: "https://www.linkedin.com/in/cheryl-wu-ba12801b1/",
+  },
+  {
+    name: "Anishka Banerjee",
+    positions: ["Executive Board Member", "VP of Finance"],
+    linkedin: "https://www.linkedin.com/in/anishka-banerjee-657775171/",
+  },
+  {
+    name: "Aryan Shah",
+    positions: ["Executive Board Member", "VP of Marketing"],
+    linkedin: "www.linkedin.com/in/aryan-shah1",
+  },
+  {
+    name: "Helena Su",
+    positions: ["Executive Board Member", "VP of Resources"],
+    linkedin: "www.linkedin.com/in/helena-su-063965193",
+  },
+  {
+    name: "Parth Asawa",
+    positions: ["Executive Board Member", "Director of FLIP"],
+    linkedin: "https://www.linkedin.com/in/pgasawa/",
+  },
+  {
+    name: "Arun Balaji",
+    positions: ["Executive Board Member", "Director of Expansion"],
+  },
+  {
+    name: "Marcus Kuo",
+    positions: ["Executive Board Member", "Human Resources Manager"],
+    linkedin: "https://www.linkedin.com/in/marcus-kuo-b9a4281aa/",
+  },
+  {
+    name: "Mason Wang",
+    positions: ["Tech Director"],
+    linkedin: "https://www.linkedin.com/in/mason-wang/",
+    imgURL:
+      "https://res.cloudinary.com/masonwang/image/upload/v1599418850/yei-website/team/mason-wang.png",
+  },
+  {
+    name: "June Wang",
+    positions: ["Grant/Sponsorship Coordinator"],
+    linkedin: "https://www.linkedin.com/in/june-wang/",
+  },
+  {
+    name: "Alysa Xu",
+    positions: ["Events Coordinator"],
+    linkedin: "https://www.linkedin.com/in/alysa-xu-06bba21aa/",
+  },
+  {
+    name: "Srushti Patil",
+    positions: ["Events Coordinator"],
+    linkedin: "https://www.linkedin.com/in/srushti-patil/",
+  },
+  {
+    name: "Deeya Jain",
+    positions: ["Events Coordinator"],
+    linkedin: "https://www.linkedin.com/in/deeya-jain/",
+  },
+  {
+    name: "Tarun Srivasta",
+    positions: ["FLIP Curriculum Designer"],
+    linkedin: "https://www.linkedin.com/in/tarun-srivastava-b9a7aa1a9/",
+  },
+  {
+    name: "Faaris Zuberi",
+    positions: ["FLIP Curriculum Designer"],
+    linkedin: "https://www.linkedin.com/in/faaris-zuberi-4207a91b0/",
+  },
+  {
+    name: "Sacha Toberoff",
+    positions: ["FLIP Partnership Manager"],
+    linkedin: "https://www.linkedin.com/in/sacha-toberoff-668a911b3/",
+  },
+  {
+    name: "Anand Singh",
+    positions: ["FLIP Partnership Manager"],
+    linkedin: "https://www.linkedin.com/in/anand-singh-321817135/",
+  },
+  {
+    name: "Nevin Thombre",
+    positions: ["FLIP Partnership Manager"],
+    linkedin: "https://www.linkedin.com/in/nevin-thombre-5154411a9/ ",
+  },
+  {
+    name: "Saachi Kumar",
+    positions: ["Curriculum Designer"],
+    linkedin: "https://www.linkedin.com/in/saachi-kumar/",
+  },
+  {
+    name: "Sanjit Pingili",
+    positions: ["Curriculum Designer"],
+    linkedin: "http://linkedin.com/in/sanjit-pingili-14b04a1aa/",
+  },
+  {
+    name: "Ethan Lin",
+    positions: ["Curriculum Designer"],
+    linkedin: "https://www.linkedin.com/in/ethan-lin-a93ab7172/",
+  },
+  {
+    name: "Sajiv Shah",
+    positions: ["Marketing Specialist"],
+    linkedin: "https://www.linkedin.com/in/sajiv-shah-b86303169/",
+  },
+  {
+    name: "Amy Zhou",
+    positions: ["Marketing Specialist"],
+    linkedin: "http://linkedin.com/in/amy-zhou-933536181/",
+  },
+  {
+    name: "Mounica Policharla",
+    positions: ["Expansion Associate", "Regional Ambassador"],
+    linkedin: "www.linkedin.com/in/mounica-policharla-3308ab1ab/",
+  },
+  {
+    name: "Brook Chuang",
+    positions: ["Expansion Associate"],
+    linkedin: "www.linkedin.com/in/chuangbrook/",
+  },
+  {
+    name: "Kavin Shah",
+    positions: ["Leadership Associate"],
+    linkedin: "www.linkedin.com/in/kavinshah-student/",
+  },
+  {
+    name: "Kevin Lin",
+    positions: ["Regional Ambassador"],
+    linkedin: "www.linkedin.com/in/kevin-lin-0274b21a7/",
+  },
+  {
+    name: "Rohan Bajpai",
+    positions: ["Regional Ambassador"],
+    linkedin: "www.linkedin.com/in/rohan-bajpai-460b141ab/",
+  },
+  {
+    name: "Neha Basu",
+    positions: ["Regional Ambassador"],
+    linkedin: "www.linkedin.com/in/nehabasu1/",
+  },
+  {
+    name: "Selina Chen",
+    positions: ["Multimedia Coordinator"],
+    linkedin: "https://www.linkedin.com/in/anushkadde/",
+  },
+  {
+    name: "Benjamin Bray",
+    positions: ["Multimedia Workflow Manager"],
+    linkedin: "https://www.linkedin.com/in/benjamin-bray-6aa9371a3/",
+  },
+  {
+    name: "Charlotte Lau",
+    positions: ["Graphic Designer"],
+    linkedin: "https://www.linkedin.com/in/charlotte-lau-8b77421b2/",
+  },
+  {
+    name: "Benjamin Chen",
+    positions: ["Finance"],
+    linkedin: "https://www.linkedin.com/in/benjamin-chen-60584a157/",
+  },
+];
+
+export default team;
