4ab4d3fe9e7f6494e25da5429a55020aa859969c
MasonWang025 <mason.wang0025@gmail.com>
2020-09-07 16:55:30 -0700

    chapters.js

---
+++ src/data/chapter.js
@@ -0,0 +1,179 @@
+const chapters = [
+  {
+    chapter: "Saratoga High School",
+    email: "henryweng2003@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "Lynbrook High School",
+    email: "ian@theyei.org",
+    region: "Norcal",
+  },
+  {
+    chapter: "Newton South High School",
+    email: "li.andrew.ch@gmail.com",
+    region: "Massachusetts",
+  },
+  {
+    chapter: "Harker Oeconomica",
+    email: "21arushis@students.harker.org",
+    region: "Norcal",
+  },
+  {
+    chapter: "Monta Vista High School",
+    email: "maggieyy7@gmail.com ",
+    region: "Norcal",
+  },
+  {
+    chapter: "Waubonsie Valley High School",
+    email: "ariondey@gmail.com",
+    region: "Illinois",
+  },
+  {
+    chapter: "Eastlake High School",
+    email: "s-ansingh@lwsd.org",
+    region: "Washington",
+  },
+  {
+    chapter: "Joaquin Miller Middle School",
+    email: "yzhoucalvin@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "American High School",
+    email: "sathwik.eshwar05@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "Chaparral High School",
+    email: "rkshishodia4@gmail.com",
+    region: "Socal",
+  },
+  {
+    chapter: "Ballantyne",
+    email: "linkevin465@gmail.com",
+    region: "North Carolina",
+  },
+  {
+    chapter: "Vaels International School",
+    email: "2003pallavi2003@gmail.com",
+    region: "India",
+  },
+  {
+    chapter: "Westwood High School",
+    email: "rohanbajpai024@gmail.com",
+    region: "Texas",
+  },
+  {
+    chapter: "GEMS Wellington Academy Silicon Oasis",
+    email: "rajvirkohli0@gmail.com",
+    region: "United Arab Emirates",
+  },
+  {
+    chapter: "BASIS Scottsdale",
+    email: "nbasu.456@gmail.com",
+    region: "Arizona",
+  },
+  {
+    chapter: "Richard Montgomery High School",
+    email: "dannyshao2013@gmail.com",
+    region: "Maryland",
+  },
+  {
+    chapter: "Green Hope High School",
+    email: "bhzhang4@gmail.com",
+    region: "North Carolina",
+  },
+  {
+    chapter: "Redwood Middle School",
+    email: "cheryl@theyei.org",
+    region: "Norcal",
+  },
+  {
+    chapter: "Palo Alto High School",
+    email: "mh33653@pausd.us",
+    region: "Norcal",
+  },
+  {
+    chapter: "Homestead High School",
+    email: "andraaliu@gmail.com\n",
+    region: "Norcal",
+  },
+  {
+    chapter: "Cupertino High School",
+    email: "alysaxu10295@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "El Camino Fundamental High School",
+    email: "marinjanedolan@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "Menlo High School",
+    email: "connor.yu@menloschool.org",
+    region: "Norcal",
+  },
+  {
+    chapter: "Archbishop Mitty High School",
+    email: "anirudhlahiri314@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "Dublin High School",
+    email: "forevergrace.15@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "John P. Stevens High School",
+    email: "snehanpatibandla@gmail.com",
+    region: "New Jersey",
+  },
+  {
+    chapter: "Leigh High School",
+    email: "jhatanmay2005@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "Viewpoint School",
+    email: "j.yang22@viewpoint.org",
+    region: "Socal",
+  },
+  {
+    chapter: "Frisco Heritage High School",
+    email: "rithikduvva@gmail.com",
+    region: "Texas",
+  },
+  {
+    chapter: "Lower Merion High School",
+    email: "jonathaninnovations@gmail.com",
+    region: "Pennsylvania",
+  },
+  {
+    chapter: "Dougherty Valley High School",
+    email: "ishaanbansal1001@gmail.com",
+    region: "Norcal",
+  },
+  {
+    chapter: "Seven Lakes High School",
+    email: "siddharthayyagari1@gmail.com",
+    region: "Texas",
+  },
+  {
+    chapter: "Sherwood Hall Senior Secondary School",
+    email: "rafahasma@icloud.com",
+    region: "India",
+  },
+  {
+    chapter: "Central High School",
+    email: "robineluvathingal@gmail.com",
+    region: "Arkansas",
+  },
+  {
+    chapter: "Lange Ley HS",
+    email: "martinakurchan@gmail.com",
+    region: "Argentina",
+  },
+];
+
+export default chapters;
