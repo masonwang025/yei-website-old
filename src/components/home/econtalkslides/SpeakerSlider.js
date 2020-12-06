701d27511e3d00c6a335ac6ef4adfcba0e0d7e1e
MasonWang025 <mason.wang0025@gmail.com>
2020-12-05 20:33:00 -0800

    temporary carousel

--- src/components/home/econtalkslides/SpeakerSlider.js
+++ src/components/home/econtalkslides/SpeakerSlider.js
@@ -1,10 +1,6 @@
 import React from "react";
-import Carousel, {
-  slidesToShowPlugin,
-  arrowsPlugin,
-  autoplayPlugin,
-} from "@brainhubeu/react-carousel";
-import "@brainhubeu/react-carousel/lib/style.css";
+
+import Carousel from "react-elastic-carousel";
 
 import { IconButton } from "@material-ui/core";
 import ArrowBack from "@material-ui/icons/ArrowBackIos";
@@ -13,89 +9,54 @@ import ArrowForward from "@material-ui/icons/ArrowForwardIos";
 import speakers from "../../../data/people/speakers";
 import SlideItem from "./SlideItem";
 
-import SlidesErrorBoundary from "./SlidesErrorBoundary";
-
-export default function SpeakerSlider(props) {
-  const universalPlugins = [
-    "infinite",
-    {
-      resolve: arrowsPlugin,
-      options: {
-        arrowLeft: (
-          <IconButton aria-label="left">
-            <ArrowBack style={{ color: "#aaa" }} />
-          </IconButton>
-        ),
-        arrowRight: (
-          <IconButton aria-label="right">
-            <ArrowForward style={{ color: "#aaa" }} />
-          </IconButton>
-        ),
-        addArrowClickHandler: true,
-      },
-    },
-    {
-      resolve: autoplayPlugin,
-      options: {
-        interval: 3000,
-      },
-    },
+export default function SpeakerSlider() {
+  const bp = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 850, itemsToShow: 3 },
   ];
 
   return (
-    <SlidesErrorBoundary>
-      <Carousel
-        animationSpeed={1000}
-        plugins={[
-          ...universalPlugins,
-          {
-            resolve: slidesToShowPlugin,
-            options: {
-              numberOfSlides: 3,
-            },
-          },
-        ]}
-        breakpoints={{
-          960: {
-            plugins: [
-              ...universalPlugins,
-              {
-                resolve: slidesToShowPlugin,
-                options: {
-                  numberOfSlides: 1,
-                },
-              },
-            ],
-          },
-          1280: {
-            plugins: [
-              ...universalPlugins,
-              {
-                resolve: slidesToShowPlugin,
-                options: {
-                  numberOfSlides: 2,
-                },
-              },
-            ],
-          },
-        }}
-      >
-        {speakers.slice(0, 10).map((speaker) => {
-          let key = speaker.name.replace(/[\W_]+/g, "-").toLowerCase();
-          let imgURL = speaker.imgURL
-            ? speaker.imgURL
-            : `https://res.cloudinary.com/masonwang/image/upload/v1600135590/yei-website/speakers/${key}.jpg`;
-          return (
-            <SlideItem
-              name={speaker.name}
-              date={speaker.date}
-              imgURL={imgURL}
-              occupation={speaker.occupation}
-              vid={speaker.vids ? speaker.vids[0] : null}
-            />
-          );
-        })}
-      </Carousel>
-    </SlidesErrorBoundary>
+    <Carousel breakPoints={bp} renderArrow={Arrow} pagination={false}>
+      {speakers.slice(0, 10).map((speaker) => {
+        let key = speaker.name.replace(/[\W_]+/g, "-").toLowerCase();
+        let imgURL = speaker.imgURL
+          ? speaker.imgURL
+          : `https://res.cloudinary.com/masonwang/image/upload/v1600135590/yei-website/speakers/${key}.jpg`;
+        return (
+          <SlideItem
+            key={key}
+            name={speaker.name}
+            date={speaker.date}
+            imgURL={imgURL}
+            occupation={speaker.occupation}
+            vid={speaker.vids ? speaker.vids[0] : null}
+          />
+        );
+      })}
+    </Carousel>
+  );
+}
+
+function Arrow({ type, onClick, isEdge }) {
+  const buttonStyle = {
+    color: isEdge ? "#555" : "#E0E0E0",
+    transition: "1.2s all",
+  };
+  const pointer =
+    type === "PREV" ? (
+      <ArrowBack style={buttonStyle} />
+    ) : (
+      <ArrowForward style={buttonStyle} />
+    );
+  return (
+    <IconButton
+      disableRipple
+      onClick={onClick}
+      disabled={isEdge}
+      style={{ background: "transparent" }}
+    >
+      {pointer}
+    </IconButton>
   );
 }
